5eadc2c699f213c2dd0f942c44c90dce4b6bcb1a
mingdu9 <min035759@gmail.com>
2023-03-08 01:09:21 +0900

    feat: get letter 구현
    특정 편지 조회
    특정 멤버와 주고 받은 편지들 조회 구현

--- back/src/main/java/com/mainproject/back/letter/repository/LetterRepository.java
+++ back/src/main/java/com/mainproject/back/letter/repository/LetterRepository.java
@@ -1,8 +1,15 @@
 package com.mainproject.back.letter.repository;
 
 import com.mainproject.back.letter.entity.Letter;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface LetterRepository extends JpaRepository<Letter, Long> {
+
+  @Query("select l from Letter l join l.member m join l.receiver r where m.memberId = :memberId or r.memberId = :receiverId")
+  Page<Letter> findLettersByMemberOrReceiver(@Param("memberId") long memberId, @Param("receiverId") long receiverId, Pageable pageable);
 
 }
