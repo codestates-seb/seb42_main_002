796f3d920d9d6bed2b3f3529ebaec757b9ce5904
hanseungwan1 <seungwanhan542@gmail.com>
2023-03-08 22:56:20 +0900

    feat:인트로 페이지 작업중

--- front/src/components/Intro/Intro.tsx
+++ front/src/components/Intro/Intro.tsx
@@ -1,12 +1,36 @@
-import React from 'react';
-import { useAuth } from '../../context/auth-context';
-import { signInUser } from '../../dummy/users';
+// import React from 'react';
+// import { useAuth } from '../../context/auth-context';
+// import { signInUser } from '../../dummy/users';
+import styles from './Intro.module.scss';
+import { ReactComponent as MailIcon } from '../../assets/Intro/mailIcon.svg';
+import { ReactComponent as OwlIcon } from '../../assets/Intro/owlIcon.svg';
+import { ReactComponent as SpeechBubble } from '../../assets/Intro/speechBubble.svg';
+import { ReactComponent as SignupBtn } from '../../assets/Intro/signupBtn.svg';
+import { ReactComponent as LoginBtn } from '../../assets/Intro/loginBtn.svg';
 
-const Intro = () => {
-  const { login } = useAuth();
+const Intro = (): JSX.Element => {
+  // const { login } = useAuth();
   return (
-    <div>
-      <button onClick={() => login(signInUser)}>로그인</button>
+    <div className={styles.intro_container}>
+      {/* <button onClick={() => login(signInUser)}>로그인</button> */}
+      <div className={styles.intro_title}>
+        <MailIcon className={styles.mail_icon} />
+        <p>전 세계에 마음이 맞는 친구들에게</p>
+        <p>당신의 편지를 전달해드립니다.</p>
+      </div>
+      <div className={styles.intro_main}>
+        <SpeechBubble />
+        <OwlIcon />
+        <p>Owls Letters</p>
+      </div>
+      <button>
+        <p>회원가입</p>
+        <SignupBtn />
+      </button>
+      <button>
+        <p>로그인</p>
+        <LoginBtn />
+      </button>
     </div>
   );
 };
