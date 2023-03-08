2d2f1f2ec7566b90110cbe0226ed94426ed866bd
12Ahn22 <12ayo22@gmail.com>
2023-03-08 11:55:24 +0900

    feat: 메시지 상태 아이콘 마크업 완성

---
+++ front/src/components/Common/LetterStatusIcon/LetterStatusIcon.tsx
@@ -0,0 +1,22 @@
+import { ReactComponent as Receive } from '../../../assets/img/letter_receive.svg';
+import { ReactComponent as Send } from '../../../assets/img/letter_send.svg';
+import { LETTER_STATUS } from '../../../utils/enums/letter.enum';
+
+import styles from './LetterStatusIcon.module.scss';
+
+type LetterStatusIconProps = {
+  status: LETTER_STATUS;
+};
+
+const LetterStatusIcon = () => {
+  return (
+    <div className={styles.letter_state}>
+      {/* 상태에 따른 아이콘 */}
+      <div className={styles.read_flag}></div>
+      <Receive />
+      {/* <Send /> */}
+    </div>
+  );
+};
+
+export default LetterStatusIcon;
