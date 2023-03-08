fbbdc9315c1cab76bede600d79ddf1bd400a94a1
구민주 <74780404+mingdu9@users.noreply.github.com>
2023-03-08 13:40:33 +0900

    Revert "Revert 16 be feat/letter crud ming"

--- back/src/main/java/com/mainproject/back/follow/entity/Follow.java
+++ back/src/main/java/com/mainproject/back/follow/entity/Follow.java
@@ -1,31 +1,38 @@
 package com.mainproject.back.follow.entity;
 
+import com.mainproject.back.audit.Auditable;
 import com.mainproject.back.member.entity.Member;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
 @Getter
-@Setter
-@NoArgsConstructor
-public class Follow {
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+public class Follow extends Auditable {
+
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
-  private int followId;
+  private Long followId;
 
-  @ManyToOne
-  @JoinColumn(name = "FOLLOWER_ID")
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "follower_id")
   private Member follower;
 
-  @ManyToOne
-  @JoinColumn(name = "FOLLOWING_ID")
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "following_id")
   private Member following;
 
 }
