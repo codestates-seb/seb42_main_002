b1faa757320fe806cf786bd9a384be3efeaa94fa
yedin123 <aas9919@naver.com>
2023-03-10 10:22:21 +0900

    feat:tag기능 추가

--- back/src/main/java/com/mainproject/back/member/controller/MemberController.java
+++ back/src/main/java/com/mainproject/back/member/controller/MemberController.java
@@ -3,10 +3,13 @@ package com.mainproject.back.member.controller;
 import com.mainproject.back.helper.UriCreator;
 import com.mainproject.back.member.dto.MemberDto;
 import com.mainproject.back.member.dto.MemberDto.Patch;
+import com.mainproject.back.member.dto.MemberTagDto;
+import com.mainproject.back.member.dto.MemberTagPatchDto;
 import com.mainproject.back.member.entity.Member;
 import com.mainproject.back.member.entity.Member.MemberStatus;
 import com.mainproject.back.member.mapper.MemberMapper;
 import com.mainproject.back.member.service.MemberService;
+import com.mainproject.back.member.tag.MemberTag;
 import java.net.URI;
 import javax.validation.Valid;
 import javax.validation.constraints.Positive;
@@ -40,11 +43,11 @@ public class MemberController {
 
   @PostMapping
   public ResponseEntity postMember(@Valid @RequestBody MemberDto.Post requestBody) {
+
     requestBody.setMemberStatus(MemberStatus.MEMBER_ACTIVE);
     Member member = mapper.memberPostToMember(requestBody);
 
     Member createdMember = memberService.createMember(member);
-
 
     URI location = UriCreator.createUri(MEMBER_DEFAULT_URL, createdMember.getMemberId());
 
@@ -56,11 +59,21 @@ public class MemberController {
   public ResponseEntity patchMember(
       @PathVariable("member-id") @Positive long memberId,
       @Valid @RequestBody MemberDto.Patch requestBody) {
-    Patch.builder()
-        .memberId(memberId)
-        .build();
+//    Patch.builder()
+//        .memberId(memberId)
+//        .build();
 
-    Member updateMember = memberService.updateMember(mapper.memberPatchToMember(requestBody));
+    requestBody.setMemberId(memberId);
+    Member updateMember = memberService.updateMember(requestBody, mapper.memberPatchToMember(requestBody));
+
+    return new ResponseEntity<>(mapper.memberToMemberResponse(updateMember), HttpStatus.OK);
+  }
+  @PatchMapping("/{member-id}/tag")
+  public ResponseEntity patchMemberTag(
+      @PathVariable("member-id") @Positive long memberId,
+      @Valid @RequestBody MemberTagPatchDto requestBody) {
+
+    Member updateMember = memberService.updateMemberTag(memberId, requestBody);
 
     return new ResponseEntity<>(mapper.memberToMemberResponse(updateMember), HttpStatus.OK);
   }
--- back/src/main/java/com/mainproject/back/member/service/MemberService.java
+++ back/src/main/java/com/mainproject/back/member/service/MemberService.java
@@ -1,10 +1,18 @@
 package com.mainproject.back.member.service;
 
 
+import com.mainproject.back.member.dto.MemberDto;
+import com.mainproject.back.member.dto.MemberTagDto;
+import com.mainproject.back.member.dto.MemberTagPatchDto;
 import com.mainproject.back.member.entity.Member;
 
 import com.mainproject.back.member.repository.MemberRepository;
+import com.mainproject.back.member.repository.MemberTagRepository;
+import com.mainproject.back.member.tag.MemberTag;
+import com.mainproject.back.tag.repository.TagRepository;
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Isolation;
@@ -15,7 +23,10 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 @Service
 public class MemberService {
+
   private final MemberRepository memberRepository;
+  private final MemberTagRepository memberTagRepository;
+  private final TagRepository tagRepository;
 
 
   public Member createMember(Member member) {
@@ -24,23 +35,49 @@ public class MemberService {
   }
 
   @Transactional(propagation = Propagation.REQUIRED, isolation = Isolation.SERIALIZABLE)
-  public Member updateMember(Member member) {
+  public Member updateMember(MemberDto.Patch requestBody, Member member) {
     Member findMember = findVerifiedMember(member.getMemberId());
 
+//    Optional.ofNullable(member.getName())
+//        .ifPresent(name -> Member.builder().name(name).build());
+//    Optional.ofNullable(member.getPassword())
+//        .ifPresent(password -> Member.builder().password(password).build());
+//    Optional.ofNullable(member.getMemberLanguages())
+//            .ifPresent(memberLanguages -> Member.builder().memberLanguages(memberLanguages).build());
+//    Optional.ofNullable(member.getMemberTags())
+//        .ifPresent(memberTags -> Member.builder().memberTags(memberTags).build());
+//    Optional.ofNullable(member.getIntroduce())
+//            .ifPresent(introduce -> Member.builder().introduce(introduce).build());
+//    Optional.ofNullable(member.getMemberStatus())
+//        .ifPresent(memberStatus -> Member.builder().memberStatus(memberStatus).build());
+
     Optional.ofNullable(member.getName())
-        .ifPresent(name -> Member.builder().name(name).build());
-    Optional.ofNullable(member.getPassword())
-        .ifPresent(password -> Member.builder().password(password).build());
-    Optional.ofNullable(member.getMemberLanguages())
-            .ifPresent(memberLanguages -> Member.builder().memberLanguages(memberLanguages).build());
-    Optional.ofNullable(member.getMemberTags())
-        .ifPresent(memberTags -> Member.builder().memberTags(memberTags).build());
+        .ifPresent(name -> findMember.setName(name));
     Optional.ofNullable(member.getIntroduce())
-            .ifPresent(introduce -> Member.builder().introduce(introduce).build());
-    Optional.ofNullable(member.getMemberStatus())
-        .ifPresent(memberStatus -> Member.builder().memberStatus(memberStatus).build());
+        .ifPresent(introduce -> findMember.setIntroduce(introduce));
+    Optional.ofNullable(member.getMemberLanguages())
+        .ifPresent(memberLanguages -> findMember.setMemberLanguages(memberLanguages));
+    Optional.ofNullable(member.getProfile())
+        .ifPresent(profile -> findMember.setProfile(profile));
 
     return memberRepository.save(findMember);
+  }
+
+  @Transactional(propagation = Propagation.REQUIRED, isolation = Isolation.SERIALIZABLE)
+  public Member updateMemberTag (long memberId, MemberTagPatchDto requestBody) {
+//    List<MemberTag> findMembers = memberTagRepository.findAllByMemberId(memberId);
+
+    Member findMember = findVerifiedMember(memberId);
+
+    memberTagRepository.deleteAllByMember(findMember);
+
+    for(MemberTagDto dto : requestBody.getMemberTags()){
+      MemberTag memberTag = MemberTag.builder()
+          .member(findMember)
+          .tag(tagRepository.findById(dto.getTagId())).build();
+      memberTagRepository.save(memberTag);
+    }
+    return findVerifiedMember(memberId);
   }
 
   @Transactional(readOnly = true)
@@ -66,8 +103,9 @@ public class MemberService {
 
   private void verifyExistsEmail(String email) {
     Optional<Member> member = memberRepository.findByEmail(email);
-    if (member.isPresent())
+    if (member.isPresent()) {
       throw new RuntimeException("MEMBER_EXISTS");
+    }
   }
 
 }
