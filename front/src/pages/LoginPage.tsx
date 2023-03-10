ecf80d0a31fda5a5f547d551953e2305cfba1a34
hanseungwan1 <seungwanhan542@gmail.com>
2023-03-10 16:14:42 +0900

    feat: 로그인페이지 구글 버튼 생성

--- front/src/pages/LoginPage.tsx
+++ front/src/pages/LoginPage.tsx
@@ -2,11 +2,17 @@ import PageTitle from '../components/Common/PageTitle/PageTitle';
 import LoginMain from '../components/Login/LoginMain';
 import { ReactComponent as BackIcon } from '../assets/BackIcon.svg';
 import SignUpFooter from '../components/SignUp/SignUpFooter';
+import styles from '../components/Login/LoginMain.module.scss';
+import { FcGoogle } from 'react-icons/fc';
 
 export default function LoginPage() {
   return (
     <>
       <PageTitle title="Login" translate="로그인" prevIcon={<BackIcon />} />
+      <button className={styles.google_btn}>
+        <FcGoogle className={styles.google_icon} />
+        구글이메일로 로그인하기
+      </button>
       <LoginMain />
       <SignUpFooter
         isText="아직 가입하지 않았다면?"
