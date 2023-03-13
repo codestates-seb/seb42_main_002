7c761eb92769818b6fa5715e19eba16363dd2ccc
mingdu9 <min035759@gmail.com>
2023-03-13 10:29:09 +0900

    fix: block 수정

--- back/src/main/java/com/mainproject/back/block/service/BlockService.java
+++ back/src/main/java/com/mainproject/back/block/service/BlockService.java
@@ -1,10 +1,12 @@
 package com.mainproject.back.block.service;
 
 import com.mainproject.back.block.entity.Block;
+import com.mainproject.back.block.exception.BlockExceptionCode;
 import com.mainproject.back.block.repository.BlockRepository;
+import com.mainproject.back.exception.BusinessLogicException;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 @RequiredArgsConstructor
@@ -18,7 +20,11 @@ public class BlockService {
     return blockRepository.save(block);
   }
 
-  public void deleteBlock(long blockId) { blockRepository.deleteById(blockId);}
+  public void deleteBlock(long blockId) {
+    long foundId = blockRepository.findBlockIdByBlockId(blockId).orElseThrow(() -> new BusinessLogicException(
+        BlockExceptionCode.BLOCK_NOT_FOUND));
+    blockRepository.deleteById(foundId);
+  }
 
   public Page<Block> findBlocks(long memberId, Pageable pageable) { return blockRepository.findAllByMemberId(memberId, pageable); }
 }
