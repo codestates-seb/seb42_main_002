742684820f65b0f3dd213932898b7fdd7b689934
BRWEB <52031484+rockbell89@users.noreply.github.com>
2023-03-17 12:45:27 +0900

    fix: 메인 swiper 슬라이더 버그 수정

--- front/src/components/Main/RecomandUser/RecomandUserList.tsx
+++ front/src/components/Main/RecomandUser/RecomandUserList.tsx
@@ -15,10 +15,8 @@ const RecomandUserList = () => {
     <Swiper
       modules={[Navigation, Pagination, Scrollbar, A11y]}
       spaceBetween={16}
-      slidesPerView={5}
+      slidesPerView={'auto'}
       scrollbar={{ draggable: true }}
-      onSwiper={(swiper) => console.log(swiper)}
-      onSlideChange={() => console.log('slide change')}
       className={styles.swiper_list}
     >
       {recomandUserData.content.map((user) => (
