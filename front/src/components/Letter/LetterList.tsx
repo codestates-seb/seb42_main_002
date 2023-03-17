205fdb9a9601500049287fb63d663e048b675ac2
12Ahn22 <12ayo22@gmail.com>
2023-03-17 17:09:29 +0900

    feat: 편지 유저 리스트 페이지 API 연결 - 로딩스피너&에러 처리 제외

--- front/src/components/Letter/LetterList.tsx
+++ front/src/components/Letter/LetterList.tsx
@@ -1,13 +1,35 @@
+import { useEffect, useState } from 'react';
+import { SlEnvolopeLetter } from 'react-icons/sl';
 import { LetterUserData } from '../../utils';
-
+import { GET } from '../../utils/axios';
+import Empty from '../Common/Empty/Empty';
 import LetterStatusIcon from '../Common/LetterStatusIcon/LetterStatusIcon';
 import UserCard from '../Common/UserCard/UserCard';
 
-import { userData } from '../../dummy/userList';
-
 import styles from './LetterList.module.scss';
+// import { userData } from '../../dummy/userList';
 
 const LetterList = () => {
+  const [userList, setUserList] = useState<LetterUserData[]>([]);
+  const [page, setPage] = useState<number>(0);
+
+  /**
+   * @description API
+   */
+  const getLetterUserList = async () => {
+    try {
+      const { data } = await GET(`/letters?page=${page}&size=5`);
+      setUserList(data.content);
+    } catch (error) {
+      console.log('error');
+      // TODO: ERROR 처리 방법
+    }
+  };
+
+  useEffect(() => {
+    getLetterUserList();
+  }, []);
+
   // 렌더링 시, 데이터 fetch
   const onClickHandler = (e: React.MouseEvent<Element, MouseEvent>) => {
     console.log('아이콘 클릭');
@@ -15,9 +37,17 @@ const LetterList = () => {
     e.stopPropagation();
   };
 
+  if (userList.length === 0) {
+    return (
+      <Empty title="아직 편지를 교환한 사람이 없어요.">
+        <SlEnvolopeLetter className={styles.icon} size={'6rem'} />
+      </Empty>
+    );
+  }
+
   return (
     <ul className={styles.letter_list}>
-      {userData.map((user: LetterUserData) => (
+      {userList.map((user: LetterUserData) => (
         <UserCard
           key={user.memberId}
           {...user}
