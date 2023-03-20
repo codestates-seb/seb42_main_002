67779906df1fd8fcdcde6848fed974d1c45aae0f
hanseungwan1 <seungwanhan542@gmail.com>
2023-03-20 20:33:05 +0900

    feat:단어장 css 수정

--- front/src/components/Voca/VocaCard/VocaCard.tsx
+++ front/src/components/Voca/VocaCard/VocaCard.tsx
@@ -2,6 +2,8 @@ import { FiTrash2 } from 'react-icons/fi';
 import { BiEdit } from 'react-icons/bi';
 
 import styles from './VocaCard.module.scss';
+import { useEffect, useState } from 'react';
+import { useRef } from 'react';
 
 type VocaCardProps = {
   word: string;
@@ -18,22 +20,56 @@ const VocaCard = ({
   onDelete,
   vocabId,
 }: VocaCardProps) => {
+  const [isClick, setIsClick] = useState(false);
+  const scrollRef: any = useRef(null);
+
+  useEffect(() => {
+    if (!isClick) {
+      scrollRef.current.scrollTop = 0;
+    }
+  }, [isClick]);
+
+  const onClickHandler = () => {
+    setIsClick(!isClick);
+  };
+
   return (
-    <li className={styles.card}>
+    <li
+      className={styles.card}
+      onClick={onClickHandler}
+      onKeyUp={(event) => {
+        event.preventDefault();
+      }}
+      role="presentation"
+    >
       {/* Word */}
       <div className={styles.word}>{word}</div>
       {/* Meaning */}
-      <div className={styles.meaning}>{meaning}</div>
+      <div
+        className={isClick ? styles.meaning : styles.meaning_beforeclick}
+        ref={scrollRef}
+      >
+        {meaning}
+      </div>
       {/* 버튼들 */}
       <div className={styles.buttons}>
         {/* 수정 */}
-        <button className={styles.button} onClick={onEdit}>
+        <button
+          className={styles.button}
+          onClick={(event) => {
+            event.stopPropagation();
+            onEdit();
+          }}
+        >
           <BiEdit size="1.125rem" />
         </button>
         {/* 삭제 */}
         <button
           className={styles.button}
-          onClick={onDelete.bind(null, vocabId)}
+          onClick={(event) => {
+            event.stopPropagation();
+            onDelete.bind(null, vocabId)();
+          }}
         >
           <FiTrash2 size="1.125rem" />
         </button>
