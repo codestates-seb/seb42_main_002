99aa75398862f3736cc1d0a357beed02b3869ff2
leegri <bestest0415@gmail.com>
2023-03-20 11:52:06 +0900

    be_feat/vocabs_Random

--- back/src/main/java/com/mainproject/back/BackApplication.java
+++ back/src/main/java/com/mainproject/back/BackApplication.java
@@ -6,7 +6,6 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableJpaAuditing
-@EnableScheduling
 @SpringBootApplication
 public class BackApplication {
 
