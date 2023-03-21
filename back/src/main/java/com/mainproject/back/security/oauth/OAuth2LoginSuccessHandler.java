12457346de09e11aa7c30e9a4aefaa3064cfdd3b
mingdu9 <min035759@gmail.com>
2023-03-21 17:52:07 +0900

    fix: redirect uri

--- back/src/main/java/com/mainproject/back/security/oauth/OAuth2LoginSuccessHandler.java
+++ back/src/main/java/com/mainproject/back/security/oauth/OAuth2LoginSuccessHandler.java
@@ -2,7 +2,6 @@ package com.mainproject.back.security.oauth;
 
 import com.mainproject.back.security.jwt.JwtTokenizer;
 import java.io.IOException;
-import java.net.URI;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
@@ -14,9 +13,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
-import org.springframework.util.LinkedMultiValueMap;
-import org.springframework.util.MultiValueMap;
-import org.springframework.web.util.UriComponentsBuilder;
 
 @Slf4j
 @Component
@@ -29,33 +25,20 @@ public class OAuth2LoginSuccessHandler extends SimpleUrlAuthenticationSuccessHan
   public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
       Authentication authentication) throws IOException, ServletException {
     log.info("OAuth2 Login 성공!");
-    try {
-      CustomOAuth2User oAuth2User = (CustomOAuth2User) authentication.getPrincipal();
+    CustomOAuth2User oAuth2User = (CustomOAuth2User) authentication.getPrincipal();
 
-      String accessToken = delegateAccessToken(oAuth2User);
-      String refreshToken = delegateRefreshToken(oAuth2User);
+    String accessToken = delegateAccessToken(oAuth2User);
+    String refreshToken = delegateRefreshToken(oAuth2User);
 
-      response.setHeader("Authorization", "Bearer " + accessToken);
-      response.setHeader("Refresh", refreshToken);
-      response.addHeader("Access-Control-Expose-Headers", "Authorization, Refresh");
-      URI redirectURI = createURI(accessToken, refreshToken);
-      log.info("## {} 리다이렉트", redirectURI);
-      getRedirectStrategy().sendRedirect(request, response, redirectURI.toString());
+    response.setHeader("Authorization", "Bearer " + accessToken);
+    response.setHeader("Refresh", refreshToken);
+    response.addHeader("Access-Control-Expose-Headers", "Authorization, Refresh");
+    String redirectURI = "http://localhost:3000";
+    log.info("## 리다이렉트 -> {}", redirectURI);
+    log.info("## 토큰: {}", accessToken);
+    getRedirectStrategy().sendRedirect(request, response,
+        "http://localhost:3000" + "?access=" + accessToken + "&refresh=" + refreshToken);
 
-    } catch (Exception e) {
-      throw e;
-    }
-  }
-
-  private URI createURI(String accessToken, String refreshToken) {
-    MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
-    params.add("access", accessToken);
-    params.add("refresh", refreshToken);
-    return UriComponentsBuilder
-        .newInstance()
-        .path("http://localhost:3000")
-        .queryParams(params)
-        .build().toUri();
   }
 
   private String delegateAccessToken(CustomOAuth2User oAuth2User) {
