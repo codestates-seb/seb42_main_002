505fabdf9c70b632bc52f5db7b7fc2bddae67ac2
mingdu9 <min035759@gmail.com>
2023-03-22 11:41:55 +0900

    번역 get -> post

--- back/src/main/java/com/mainproject/back/letter/controller/TranslateController.java
+++ back/src/main/java/com/mainproject/back/letter/controller/TranslateController.java
@@ -5,7 +5,7 @@ import com.mainproject.back.letter.service.LetterService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,7 +16,7 @@ public class TranslateController {
 
   private final LetterService letterService;
 
-  @GetMapping("/translate")
+  @PostMapping("/translate")
   public ResponseEntity<Object> getTranslated(@RequestBody LetterTranslateDto body) {
     log.info("## 편지 번역");
     LetterTranslateDto result = letterService.translate(body);
