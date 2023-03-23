a4251d0d9658af055f65b0289dfb8a19c7669255
mingdu9 <min035759@gmail.com>
2023-03-23 16:54:48 +0900

    chore: be 브랜치에 합치기 위한 수정

---
+++ back/src/test/java/com/mainproject/back/member/repository/MemberRepositoryTest.java
@@ -0,0 +1,20 @@
+package com.mainproject.back.member.repository;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = Replace.NONE)
+public class MemberRepositoryTest {
+
+  @Autowired
+  private MemberRepository memberRepository;
+
+  @BeforeEach
+  public void init(){
+
+  }
+}
