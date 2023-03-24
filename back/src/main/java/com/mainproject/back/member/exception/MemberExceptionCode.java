120b51b9b260cea2d1bf5dc77ff5a3a9d851d675
mingdu9 <min035759@gmail.com>
2023-03-24 12:33:08 +0900

    fix: found 오류코드 conflict로 수정

--- back/src/main/java/com/mainproject/back/member/exception/MemberExceptionCode.java
+++ back/src/main/java/com/mainproject/back/member/exception/MemberExceptionCode.java
@@ -10,9 +10,9 @@ import org.springframework.http.HttpStatus;
 public enum MemberExceptionCode implements ExceptionCode {
 
 
-  MEMBER_NOT_FOUND(HttpStatus.CONFLICT, "회원을 찾을 수 없습니다."),
-  MEMBER_NOT_ALLOWED(HttpStatus.NOT_FOUND, "권한이 없습니다."),
-  EMAIL_EXISTS(HttpStatus.FOUND, "이미 존재하는 이메일입니다.");
+  MEMBER_NOT_FOUND(HttpStatus.NOT_FOUND, "회원을 찾을 수 없습니다."),
+  MEMBER_NOT_ALLOWED(HttpStatus.FORBIDDEN, "권한이 없습니다."),
+  EMAIL_EXISTS(HttpStatus.CONFLICT, "이미 존재하는 이메일입니다.");
 
   private final HttpStatus status;
   private final String message;
