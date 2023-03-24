df03585c5182697d65b338d648f9dff745d1eb39
12Ahn22 <12ayo22@gmail.com>
2023-03-25 03:09:11 +0900

    feat: 블랙 리스트 삭제 기능 보완 삭제 버튼 클릭 시, 확인 모달창 삭제 시, 페이지 리렌더링

--- front/src/components/BlackList/BlackList.tsx
+++ front/src/components/BlackList/BlackList.tsx
@@ -1,7 +1,9 @@
-import { Suspense, useEffect } from 'react';
+import { Suspense, useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useRecoilState, useSetRecoilState } from 'recoil';
 import { TiDeleteOutline } from 'react-icons/ti';
+import { FiUsers } from 'react-icons/fi';
+import AlertModal from '../Common/Modal/AlertModal';
 import { blackListState } from '../../recoil/atoms';
 import { blackListStateType, BlackUserDataType } from '../../utils';
 import { DELETE } from '../../utils/axios';
@@ -11,12 +13,13 @@ import { blackListSeletor } from '../../recoil/selectors/user/blacklist';
 import UserCard from '../Common/UserCard/UserCard';
 import NextUserCardList from '../Common/UserCard/NextUserCardList';
 import styles from './BlackList.module.scss';
-import { FiUsers } from 'react-icons/fi';
 
 const BlackList = () => {
   const navigate = useNavigate();
   const setPagination = useSetRecoilState(pageNationState);
   const [blackUserList, setBlackUserList] = useRecoilState(blackListState);
+  const [isAlertOpen, setIsAlertOpen] = useState<boolean>(false);
+  const [deleteMemberId, setDeleteMemberId] = useState<number>();
 
   const addRecentData = (data: blackListStateType) => {
     setBlackUserList((prev) => ({
@@ -40,16 +43,23 @@ const BlackList = () => {
     return resetBlackList;
   }, []);
 
-  // API 연결
-  const deleteBlackListHandler = async (
-    e: React.MouseEvent<HTMLButtonElement>,
+  /**
+   * @description 차단 취소 API
+   */
+  const openAlertModalHandler = (
+    event: React.MouseEvent<HTMLButtonElement>,
     targetId: number
   ) => {
-    e.stopPropagation();
+    event.stopPropagation();
+    setIsAlertOpen(true);
+    setDeleteMemberId(targetId);
+  };
+  const deleteBlackListHandler = async () => {
     try {
-      const request = await DELETE(`/users/me/blocks?target=${targetId}`);
+      const request = await DELETE(`/users/me/blocks?target=${deleteMemberId}`);
       if (request) {
-        console.log(request);
+        resetBlackList();
+        setIsAlertOpen(false);
       }
     } catch (error) {
       console.error(error);
@@ -64,7 +74,7 @@ const BlackList = () => {
     return (
       <button
         className={styles.button}
-        onClick={(event) => deleteBlackListHandler(event, memberId)}
+        onClick={(event) => openAlertModalHandler(event, memberId)}
       >
         <TiDeleteOutline size="1.7rem" />
       </button>
@@ -78,6 +88,18 @@ const BlackList = () => {
 
   return (
     <>
+      {isAlertOpen && (
+        <AlertModal
+          labelClose="닫기"
+          labelSubmit="삭제"
+          onClose={() => {
+            setIsAlertOpen(false);
+          }}
+          onSubmit={deleteBlackListHandler}
+        >
+          차단을 취소하시겠습니까?
+        </AlertModal>
+      )}
       <ul className={styles.letter_list}>
         {blackUserList.content.map((user: BlackUserDataType) => (
           <UserCard
@@ -89,12 +111,7 @@ const BlackList = () => {
             date={null}
             onClick={moveProfileHandler}
           >
-            <button
-              className={styles.button}
-              onClick={(event) => deleteBlackListHandler(event, user.memberId)}
-            >
-              <TiDeleteOutline size="1.7rem" />
-            </button>
+            <DeleteBlackListButton />
           </UserCard>
         ))}
         {/* 새로 불러오는 List */}
