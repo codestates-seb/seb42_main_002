ae3ec4e0801d7b87d1f914230cdddf1616261c14
mingdu9 <min035759@gmail.com>
2023-03-24 23:40:03 +0900

    fix: follow left join 수정

--- back/src/main/java/com/mainproject/back/member/repository/MemberRepository.java
+++ back/src/main/java/com/mainproject/back/member/repository/MemberRepository.java
@@ -15,14 +15,15 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
 
   Optional<Member> findByEmail(String email);
 
-  @Query("select m.memberId from Member m where m.email = :email")
+  @Query(value = "select m.member_id from member m where m.email = :email and m.member_status = \"MEMBER_ACTIVE\"", nativeQuery = true)
   Optional<Long> findMemberIdByEmail(@Param("email") String email);
 
   // 공통된 태그 많은 순으로 정렬
-  @Query(value = "select m.* from member_tag a join member_tag b on a.tag_id = b.tag_id "
-      + "join member m on b.member_id = m.member_id "
-      + "where a.member_id = :memberId and b.member_id != :memberId and member_status = \"MEMBER_ACTIVE\" "
-      + "group by b.member_id order by count(*) desc", nativeQuery = true)
+  @Query(value = "select m.* from member_tag as mt "
+      + "join member as m on m.member_id = mt.member_id "
+      + "where mt.tag_id in(select t.tag_id from member_tag as t where t.member_id = :memberId) and mt.member_id != :memberId "
+      + "and m.member_status =  \"MEMBER_ACTIVE\" "
+      + "group by m.member_id order by count(mt.tag_id) desc", nativeQuery = true)
   Page<Member> findRecommended(@Param("memberId") long memberId, Pageable pageable);
 
   @Query(value = "select m.* from member m "
