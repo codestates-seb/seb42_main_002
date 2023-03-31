fcc1aa2918d7e16b912baaf22a36bb7dd43e7fcb
mingdu9 <min035759@gmail.com>
2023-03-31 14:44:51 +0900

    fix: member 생일 수정

--- back/src/main/java/com/mainproject/back/BackApplication.java
+++ back/src/main/java/com/mainproject/back/BackApplication.java
@@ -1,7 +1,5 @@
 package com.mainproject.back;
 
-import java.util.TimeZone;
-import javax.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
@@ -9,11 +7,6 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @EnableJpaAuditing
 @SpringBootApplication
 public class BackApplication {
-
-  @PostConstruct
-  public void started(){
-    TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
-  }
 
   public static void main(String[] args) {
     SpringApplication.run(BackApplication.class, args);
